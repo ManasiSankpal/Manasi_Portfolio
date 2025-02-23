9d18f724b8a3fed0a67262d86ff78ecc8aed2b2a
ManasiSankpal <manasisankpal11@gmail.com>
2025-02-23 12:11:32 +0530

    first commit

---
+++ portfolio-master/assets/js/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/**
+ * Navbar toggle
+ */
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("nav-active");
+  this.classList.toggle("active");
+});
+
+/**
+ * Scroll effect for header & back to top
+ */
+const backTopBtn = document.querySelector("[data-back-to-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+});
